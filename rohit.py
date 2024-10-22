754d4c4227b8842e0330c833ef7f3cae4f559b24
Rohit Kumar <yorohitkumartyagi7010u@gmail.com>
2024-10-22 15:47:22 +0530

    new two files have been added

---
+++ rohit.py
@@ -0,0 +1,10 @@
+class rexgalaxy:
+    def __init__(self):
+        pass
+    def my_function(self):
+        return "hi how are you?"
+my_object = rexgalaxy()
+
+if __name__ == "__main__":
+
+    print(my_object.my_function())
